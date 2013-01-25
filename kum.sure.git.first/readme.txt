43c4c237cfaa19c8b5bdaeeccd63fc828721b665
Kumar Suren <kumarsuren2@gmail.com>
2013-01-25 17:36:17 -0500

    added a new line to systemout as well as a new file readme.txt to project

---
+++ kum.sure.git.first/readme.txt
@@ -0,0 +1,3 @@
+This project is created to learn the creation of GITHub projects and 
+use a programming stack like 
+Eclipse-Egit-m2e etc
--- kum.sure.git.first/src/kum/sure/git/first/GitTest.java
+++ kum.sure.git.first/src/kum/sure/git/first/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
 		// TODO Auto-generated method stub
 
 		System.out.println("Git is yet to be mastered... so far so good");
+		System.out.println("Git is looking cool...");
 	}
 
 }
