e6297267b47c763f5d963da9658cf04912838e10
Kumar Suren <kumarsuren2@gmail.com>
2013-01-25 17:25:59 -0500

    Initial Comment while committing

---
+++ kum.sure.git.first/src/kum/sure/git/first/GitTest.java
@@ -0,0 +1,14 @@
+package kum.sure.git.first;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Git is yet to master... so far so good");
+	}
+
+}
