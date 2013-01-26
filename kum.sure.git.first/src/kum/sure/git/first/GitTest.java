72b1b8d6c814d1649ca9f9ca994625cef568c450
Kumar Suren <kumarsuren2@gmail.com>
2013-01-25 20:39:19 -0500

    A change was made locally before merging with the Github editor change

--- kum.sure.git.first/src/kum/sure/git/first/GitTest.java
+++ kum.sure.git.first/src/kum/sure/git/first/GitTest.java
@@ -17,6 +17,8 @@ public class GitTest {
 		 */
 		
 		System.out.println("At this point of time I have the project added to GitHub and adding more changes through Eclipse");
+		
+		System.out.println("This change is from Eclipse before Github change is merged back");
 	}
 
 }
