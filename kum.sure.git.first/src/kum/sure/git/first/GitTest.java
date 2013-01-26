cfac832455d56cc3a0c6fade1303d54b9ced16d8
Kumar Suren <kumarsuren2@gmail.com>
2013-01-25 20:26:05 -0500

    A new comment is added to the code

--- kum.sure.git.first/src/kum/sure/git/first/GitTest.java
+++ kum.sure.git.first/src/kum/sure/git/first/GitTest.java
@@ -11,6 +11,11 @@ public class GitTest {
 		System.out.println("Git is yet to be mastered... so far so good");
 		System.out.println("Git is looking cool...");
 		System.out.println("New ways to operate git is being learned ...1 ");
+		
+		/* 
+		 * This comment is added to change the code
+		 */
+		
 	}
 
 }
